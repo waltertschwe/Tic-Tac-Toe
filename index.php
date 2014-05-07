eac5e23f18919a7e38a0d38f679e624b579e6c5e
Walter Schweitzer <wschweitzer00@gmail.com>
2014-05-07 01:07:26 -0400

    ui enhancements w/twitter bootstrap

--- index.php
+++ index.php
@@ -3,9 +3,32 @@
 <head>
 <title>Tic Tac Toe</title>
 <link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 </head>
+<body>
 
-<div id="board">
+<div class="container">
+    <div class="page-header">
+        <h1><font color="blue">Tic-Tac-Toe</font></h1>
+    </div
+</div>
+<div class="row"></div>
+<div class="row">
+	<div class="col-xs-6 col-md-4">
+		 <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title"><span class="glyphicon glyphicon-tower"></span>&nbsp;&nbsp;How To Play</h3>
+            </div>
+            <div class="panel-body">
+                1. Player goes first <br/>
+                2. Click on a box and try to make three in a row!
+            </div>
+          </div>
+		
+	</div>
+	<div class="col-xs-6 col-md-4">
+<div id="board" style="width:400px; height:300px; margin:0 auto;">
     <table>
       <tr id="row1">
         <td id="slot1" class="square"></td>
@@ -24,8 +47,10 @@
       </tr>
     </table>
   </div>
-
+ </div>
+<div class="col-xs-6 col-md-4"><!-- Overall Results Pulled from database --></div>
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -3,23 +3,29 @@ $(document).ready(function(){
 		cellId = $(this).attr('id');
 		var result = cellId.split("t");
 		var slot   = result[1];
-			$('#'+ cellId).prepend('<div class="x-image"><img src="img/x-image.png" /></div>');
+			$('#'+ cellId).prepend('<div class="x-image"><img class="displayed" src="img/x-image.png" /></div>');
 			 $.ajax({
+			 	 async: false,
 	   			 type: 'GET',
 	      		 dataType:'json',
 	    		 url: 'controller.php',
 	   			 data: {"slot" : slot}, 
 	    		 success: function(responseData) {
-	       			 $('#slot'+responseData.aiSelection).prepend('<div class="o-image"><img  src="img/o.gif" /></div>');
+	       			 $('#slot'+responseData.aiSelection).prepend('<div class="o-image"><img class="displayed" src="img/o.gif" /></div>');
 	       			 $('#slot'+responseData.aiSelection).unbind("click");
 	   			 	 var aiWinner = responseData.aiWinner;
 	   			 	 var isDraw   = responseData.isDraw;
+	   			 	 var playerWinner = responseData.playerWinner;
 	   			 	 if(isDraw > 0) {
 	   			 	 	alert("A DRAW!!! Clearing board...");
 	   			 	 	location.reload();
 	   			 	 }
 	       			 if(aiWinner > 0) {
 	       			 	alert("AI WINS!!! Clearing board...");
+	       			 	location.reload();
+	       			 }
+	       			 if(playerWinner > 0) {
+	       			 	alert("PLAYER WINS!!! Clearing board...");
 	       			 	location.reload();
 	       			 }
 	   			 },
